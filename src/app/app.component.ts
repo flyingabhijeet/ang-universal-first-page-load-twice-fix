1d4e2ce2a1957a39c765d93d15c19870f595c26c
Abhi Raj <raj.abhisingh17@gmail.com>
2021-05-21 05:28:20 +0530

    Fix: First page loading twice on angular-universal application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'universal';
+
+export class AppComponent implements OnInit {
+  title: string = 'Universal Test App';
+  iterator: number[] = new Array(1);
+  isBrowser: any;
+  isServer: any;
+  
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any
+  ){
+    this.isBrowser = isPlatformBrowser(platformId);
+    this.isServer = isPlatformServer(platformId);
+  }
+  
+  ngOnInit() {
+    if(this.isBrowser)
+      console.log('App loaded on browser!');
+    else if(this.isServer)
+      console.log('App loaded on server!');
+    else
+      console.log('App loaded!');
+  }
+  
+  log(logMe: string) {
+    console.log(`Hello ${logMe}`);
+  }
+  
 }
